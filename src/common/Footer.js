1a001b40505541ef793573a0f0c70b0f253fe769
Julian Andrade <julifeli123@gmail.com>
2023-11-06 16:47:54 -0500

    estructura de la web terminada

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -33,7 +33,7 @@ const Footer = () => {
           className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         />
       </div>
-      <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+      <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover-bg-indigo-600 rounded">
         Button
       </button>
     </div>
@@ -68,9 +68,9 @@ const Footer = () => {
   );
 
   return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap md:text-left text-center order-first">
+    <footer className="text-gray-600 body-font text-center py-12">
+      <div className="container px-5 mx-auto">
+        <div className="flex flex-wrap md:text-left text-center">
           {/* Category Section */}
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
