6d3047b00de2d6e1e60c257ffe39516640776a96
Julian Andrade <julifeli123@gmail.com>
2023-11-06 18:35:00 -0500

    se agrega el carrousel del home

--- src/components/Imagen100.js
+++ src/components/Imagen100.js
@@ -1,55 +1,93 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Imagen100() {
-  // URL of the example image
-  const imageUrl =
-    'https://img.freepik.com/premium-photo/food-collage-white-wooden-background-assortment-dishes-drinks_187166-46114.jpg?w=826';
+  const imageUrls = [
+    'https://images.rappi.com/restaurants_background/hamburguesas27-1663618601524.jpg',
+    'https://static.nationalgeographic.es/files/styles/image_3200/public/p053lo_gettyimages_pa-1071365032.jpg?w=1600&h=900',
+    'https://content.skyscnr.com/m/2dcd7d0e6f086057/original/GettyImages-186142785.jpg?crop=1224px:647px&quality=100&position=attention',
+    // Agrega más URLs de imágenes según sea necesario
+  ];
 
-  const centerContainer = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '90vh', // Centrar verticalmente en el viewport
+  const textContent = [
+    {
+      categoryText: 'Fotografía profesional 1',
+      titleText: 'Capturar el sabor a través del 1',
+      descriptionText: 'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat 1.',
+      learnMoreText: 'Learn More 1',
+      likesText: '1.2K 1',
+      commentsText: '6 1',
+    },
+    {
+      categoryText: 'Fotografía profesional 2',
+      titleText: 'Capturar el sabor a través del 2',
+      descriptionText: 'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat 2.',
+      learnMoreText: 'Learn More 2',
+      likesText: '1.2K 2',
+      commentsText: '6 2',
+    },
+    {
+      categoryText: 'Fotografía profesional 3',
+      titleText: 'Capturar el sabor a través del 3',
+      descriptionText: 'Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat 3.',
+      learnMoreText: 'Learn More 3',
+      likesText: '1.2K 3',
+      commentsText: '6 3',
+    },
+    // Agrega más objetos de contenido de texto según sea necesario
+  ];
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500, // 1 segundo para la transición
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
-  const categoryText = 'Fotografía y Maquillaje de Alimentos';
-  const titleText = 'Capturar el sabor atraves del lente';
-  const descriptionText = 'Fotografía personalizada, Chef y maquillador de alimentos, ';
-  const learnMoreText = 'aprende mas';
-  const likesText = '1.2K';
-  const commentsText = '6';
+  const imageContainerStyle = {
+    width: '100%',
+    height: '100vh', // 100% de la altura de la pantalla
+    overflow: 'hidden', // Ocultar el exceso de la imagen
+    position: 'relative',
+  };
 
-  const squareContainerStyles = {
-    backgroundColor: 'white', // Fondo blanco
-    borderRadius: '10px', // Bordes redondeados
-    padding: '20px', // Espaciado interno
-    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.3)', // Sombra
+  const imageStyle = {
+    width: '100%',
+    height: '100%', // 100% de la altura de su contenedor
+    objectFit: 'cover', // Recortar la imagen automáticamente
+    position: 'absolute',
+    top: 0,
+    left: 0,
   };
 
   return (
-    <section className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${imageUrl})` }}>
-      <div className="container px-5 py-24 mx-auto" style={centerContainer}>
-        <div className="flex justify-center">
-          <div className="w-full md:w-1/2 lg:w-1/3">
-            <div className="h-full bg-gray-100 bg-opacity-75 rounded-lg overflow-hidden text-center relative">
-              {/* Contenedor cuadrado con fondo blanco y bordes redondeados */}
-              <div style={squareContainerStyles}>
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{categoryText}</h2>
-                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{titleText}</h1>
-                <p className="leading-relaxed mb-3">{descriptionText}</p>
-                <a className="text-indigo-500 inline-flex items-center">
-                  {learnMoreText}
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+    <Slider {...sliderSettings}>
+      {imageUrls.map((imageUrl, index) => (
+        <div key={index}>
+          <div className="container mx-auto" style={imageContainerStyle}>
+            <img src={imageUrl} alt={`Image ${index}`} style={imageStyle} />
+            <div className="w-full md:w-1/2 lg:w-1/3" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+              <div className="h-full bg-gray-100 bg-opacity-75 rounded-lg overflow-hidden text-center relative">
+                <div style={{ backgroundColor: 'white', borderRadius: '10px', padding: '20px', boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.3)' }}>
+                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{textContent[index].categoryText}</h2>
+                  <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{textContent[index].titleText}</h1>
+                  <p className="leading-relaxed mb-3">{textContent[index].descriptionText}</p>
+                  <a className="text-indigo-500 inline-flex items-center">
+                    {textContent[index].learnMoreText}
+                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      ))}
+    </Slider>
   );
 }
-
