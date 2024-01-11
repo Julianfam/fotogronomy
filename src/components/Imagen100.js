dab04d4414f107bc029a379a323fa6b0a92e5e28
Julian Andrade <julifeli123@gmail.com>
2024-01-10 19:12:49 -0500

    correccion de imagen100

--- src/components/Imagen100.js
+++ src/components/Imagen100.js
@@ -56,7 +56,7 @@ export default function Imagen100() {
       {imageUrls.map((imageUrl, index) => (
         <div key={index}>
           <div className="container mx-auto" style={imageContainerStyle}>
-            <Image src={imageUrl} alt={`Image ${index}`} fill="auto" />
+            <Image src={imageUrl} alt={`Image ${index}`}  fill="auto" style={{ objectFit: 'cover', overflow: 'hidden' }} />
             <div className="w-full md:w-1/2 lg:w-1/3" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
               <div className="h-full bg-gray-100 bg-opacity-75 rounded-lg overflow-hidden text-center relative">
                 <div
