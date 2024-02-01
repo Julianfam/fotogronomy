80021107192bd802c3766b14e561716dc7d455f5
Julian Andrade <julifeli123@gmail.com>
2024-02-01 17:30:15 -0500

    Solucionados los formularios de subscripcion y contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,4 +1,27 @@
-export default function Contacto() {
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Contacto = () => {
+  const formRef = useRef();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    try {
+      await emailjs.sendForm('service_v0nv5xo', 'template_v38heg6', formRef.current, 'LYRlOjidE5geJfm1E');
+      console.log('Email sent successfully');
+      setShowConfirmation(true);
+
+      setTimeout(() => {
+        setShowConfirmation(false);
+      }, 3000);
+    } catch (error) {
+      console.error('Error sending email:', error.text);
+      // Handle error as needed
+    }
+  };
+
   return (
     <div className="container mx-auto py-12">
       <h1 className="text-4xl font-bold text-indigo-600 mb-6">Contáctanos</h1>
@@ -12,18 +35,18 @@ export default function Contacto() {
         </div>
         <div>
           <h2 className="text-2xl font-semibold mb-2 text-indigo-600">Formulario de contacto</h2>
-          <form>
+          <form ref={formRef} onSubmit={sendEmail}>
             <div className="mb-4">
               <label htmlFor="nombre" className="block text-sm font-medium text-gray-700">
                 Nombre
               </label>
-              <input type="text" id="nombre" name="nombre" className="mt-1 p-2 border rounded w-full focus:ring focus:ring-indigo-300" placeholder="Escribe tu nombre" />
+              <input type="text" id="nombre" name="user_name" className="mt-1 p-2 border rounded w-full focus:ring focus:ring-indigo-300" placeholder="Escribe tu nombre" />
             </div>
             <div className="mb-4">
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                 Correo electrónico
               </label>
-              <input type="email" id="email" name="email" className="mt-1 p-2 border rounded w-full focus:ring focus:ring-indigo-300" placeholder="example@example.com" />
+              <input type="email" id="email" name="user_email" className="mt-1 p-2 border rounded w-full focus:ring focus:ring-indigo-300" placeholder="example@example.com" />
             </div>
             <div className="mb-4">
               <label htmlFor="mensaje" className="block text-sm font-medium text-gray-700">
@@ -31,7 +54,7 @@ export default function Contacto() {
               </label>
               <textarea
                 id="mensaje"
-                name="mensaje"
+                name="message"
                 rows="4"
                 className="mt-1 p-2 border rounded w-full focus:ring focus:ring-indigo-300"
                 placeholder="Escribenos tus dudas, inquietudes o requerimientos, pronto te respondemos."
@@ -41,8 +64,17 @@ export default function Contacto() {
               Enviar
             </button>
           </form>
+          {showConfirmation && (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+              <div className="w-24 h-24 bg-white border border-black p-2 text-center">
+                <p className="text-xs">¡Enviado!</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Contacto;
