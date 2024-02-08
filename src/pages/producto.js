f242640b61bb14ef5633f24c672d9845be3a0dcb
Julian Andrade <julifeli123@gmail.com>
2024-02-08 10:27:28 -0500

    after linting error

--- src/pages/producto.js
+++ src/pages/producto.js
@@ -1,35 +1,36 @@
 import React from 'react';
 
 // Componente de Tarjeta de Precio
-const PriceCard = ({ title, price, features, buttonText }) =>{
+const PriceCard = ({ title, price, features, buttonText }) => {
   const handleNavigation = () => {
     window.location.href = '/contacto';
-  }; return (
-  <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
-    <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-      <h2 className="text-sm tracking-widest title-font mb-1 font-medium">{title}</h2>
-      <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{price}</h1>
-      {features.map((feature, index) => (
-        <p key={index} className="flex items-center text-gray-600 mb-2">
-          <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-              <path d="M20 6L9 17l-5-5"></path>
-            </svg>
-          </span>
-          {feature}
-        </p>
-      ))}
-      <button onClick={handleNavigation} className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
-        {buttonText}
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button>
-      <p className="text-xs text-gray-500 mt-3">Literally you probably haven&apos;t heard of them jean shorts.</p>
+  };
+  return (
+    <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+      <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+        <h2 className="text-sm tracking-widest title-font mb-1 font-medium">{title}</h2>
+        <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">{price}</h1>
+        {features.map((feature, index) => (
+          <p key={index} className="flex items-center text-gray-600 mb-2">
+            <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                <path d="M20 6L9 17l-5-5"></path>
+              </svg>
+            </span>
+            {feature}
+          </p>
+        ))}
+        <button onClick={handleNavigation} className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
+          {buttonText}
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+        <p className="text-xs text-gray-500 mt-3">Literally you probably haven&apos;t heard of them jean shorts.</p>
+      </div>
     </div>
-  </div>
-);
-}
+  );
+};
 // Componente de Sección de Precios
 const producto = () => {
   // Datos de cada tarjeta de precio
--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import imgSobre from '/public/assets/images/image04.jpg';
 
-
-
 const title = 'Despierta Paladares y Miradas: Donde la Gastronomía se Convierte en Arte Visual';
 const subtitle = '¡Bienvenido al fascinante mundo de la fotografía gastronómica y visual con un toque provocativo!';
 const description = [
@@ -28,11 +26,11 @@ export default function sobre() {
           <h2 className="font-bold">{subtitle}</h2>
           <p className="mb-8 leading-relaxed">{description}</p>
           <div className="flex justify-center">
-          <div className="flex justify-center">
-            <button onClick={handleNavigation} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Dale calidad a tu imagen
-            </button>
-          </div>
+            <div className="flex justify-center">
+              <button onClick={handleNavigation} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Dale calidad a tu imagen
+              </button>
+            </div>
           </div>
         </div>
       </div>
