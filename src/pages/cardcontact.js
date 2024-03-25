ea3e0f6c9782a72ce9b83f4cb029285f498a5059
Julian Andrade <julifeli123@gmail.com>
2024-03-25 17:14:49 -0500

    correccion tarjeta de contacto, cambio de nombre a contact

--- src/pages/cardcontact.js
+++ src/pages/cardcontact.js
@@ -16,19 +16,19 @@ const ContactPage = () => {
         Fotogronomy
       </h1>
       <h3 className="text-2xl mb-8"> Este es nuestro portal de contacto</h3>
-      <Image src={imgcard} alt="Tausha 3 Salsas" style={{ height: '20vh', width: 'auto', overflow: 'hidden' }} />
+      <Image src={imgcard} alt="Tausha 3 Salsas" style={{ height: 'auto', width: '80vw', overflow: 'hidden' }} />
       <div className="flex flex-col items-center w-full py-4 ">
         <a href="https://www.fotogronomy.com" target="_blank" rel="noopener noreferrer">
-          <button className="bg-white hover:bg-gray-100 text-gray-800  font-semibold py-2 px-8 w-96 border border-gray-400 rounded-lg shadow w-90vw mb-4">WEB</button>
+          <button className="bg-white hover:bg-gray-100 text-gray-800  font-semibold py-2 px-8 w-72  border border-gray-400 rounded-lg shadow w-90vw mb-4">WEB</button>
         </a>
         <a href="https://tiktok.com/fotogronomy" target="_blank" rel="noopener noreferrer">
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-96 border border-gray-400 rounded-lg shadow w-90vw mb-4">TIKTOK</button>
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-72  border border-gray-400 rounded-lg shadow w-90vw mb-4">TIKTOK</button>
         </a>
         <a href="https://www.instagram.com/fotogronomy" target="_blank" rel="noopener noreferrer">
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-96 border border-gray-400 rounded-lg shadow w-90vw mb-4">INSTAGRAM</button>
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-72 border border-gray-400 rounded-lg shadow w-90vw mb-4">INSTAGRAM</button>
         </a>
         <a href="https://www.whatsapp/3202502648" target="_blank" rel="noopener noreferrer">
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-96 border border-gray-400 rounded-lg shadow w-90vw">WHATSAPP</button>
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-8 w-72 border border-gray-400 rounded-lg shadow w-90vw">WHATSAPP</button>
         </a>
       </div>
     </div>
